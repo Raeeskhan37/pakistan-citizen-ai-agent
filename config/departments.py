373cf8a3a251c11cd9704fd26bd07bf0ced3a71d
Raeeskhan37 <raees.bscs37@gmail.com>
2026-09-21 19:26:14 +0500

    Create departments.py

---
+++ config/departments.py
@@ -0,0 +1,177 @@
+DEPARTMENTS = {
+
+    "NADRA": {
+        "description": "CNIC, NICOP, POC, CRC, FRC and NADRA services.",
+        "keywords": [
+            "nadra",
+            "cnic",
+            "nicop",
+            "poc",
+            "crc",
+            "frc",
+            "b-form",
+            "identity card",
+            "smart card",
+            "pak identity",
+            "pakid",
+        ],
+        "official_domains": [
+            "nadra.gov.pk",
+        ],
+    },
+
+    "Passport": {
+        "description": "Passport application, renewal, fees and delivery.",
+        "keywords": [
+            "passport",
+            "renew passport",
+            "new passport",
+            "machine readable passport",
+            "mrp",
+            "urgent passport",
+            "normal passport",
+            "dgip",
+        ],
+        "official_domains": [
+            "dgip.gov.pk",
+        ],
+    },
+
+    "Union Council / Local Government": {
+        "description": "Birth, death, marriage, divorce and local government services.",
+        "keywords": [
+            "union council",
+            "birth certificate",
+            "death certificate",
+            "marriage certificate",
+            "nikah registration",
+            "divorce certificate",
+            "local government",
+            "local council",
+        ],
+        "official_domains": [
+            "gov.pk",
+            "punjab.gov.pk",
+            "sindh.gov.pk",
+            "kp.gov.pk",
+            "balochistan.gov.pk",
+        ],
+    },
+
+    "Driving Licence": {
+        "description": "Driving licence learner permit, renewal and related services.",
+        "keywords": [
+            "driving licence",
+            "driving license",
+            "learner licence",
+            "learner license",
+            "licence renewal",
+            "license renewal",
+            "driving test",
+            "dlims",
+        ],
+        "official_domains": [
+            "gov.pk",
+            "punjab.gov.pk",
+            "sindh.gov.pk",
+            "kp.gov.pk",
+            "balochistan.gov.pk",
+            "islamabad.gov.pk",
+        ],
+    },
+
+    "Arms Licence": {
+        "description": "Arms licence application, renewal and related government procedures.",
+        "keywords": [
+            "arms licence",
+            "arms license",
+            "weapon licence",
+            "weapon license",
+            "gun licence",
+            "gun license",
+        ],
+        "official_domains": [
+            "gov.pk",
+            "punjab.gov.pk",
+            "sindh.gov.pk",
+            "kp.gov.pk",
+            "balochistan.gov.pk",
+        ],
+    },
+
+    "Police Clearance": {
+        "description": "Police clearance, character certificates and related police services.",
+        "keywords": [
+            "police clearance",
+            "police character certificate",
+            "character certificate",
+            "police certificate",
+            "police verification",
+            "police clearance certificate",
+        ],
+        "official_domains": [
+            "gov.pk",
+            "punjabpolice.gov.pk",
+            "sindhpolice.gov.pk",
+            "kppolice.gov.pk",
+            "balochistanpolice.gov.pk",
+            "islamabadpolice.gov.pk",
+        ],
+    },
+
+    "Protector for Visa": {
+        "description": "Protector of Emigrants and overseas employment procedures.",
+        "keywords": [
+            "protector",
+            "protector stamp",
+            "protector of emigrants",
+            "emigration",
+            "overseas employment",
+            "beoe",
+            "bureau of emigration",
+            "visa protector",
+        ],
+        "official_domains": [
+            "beoe.gov.pk",
+        ],
+    },
+
+    "Vaccination for Travelling Abroad": {
+        "description": "Travel vaccination and health requirements for international travel.",
+        "keywords": [
+            "vaccination",
+            "vaccine",
+            "travel vaccination",
+            "yellow fever",
+            "meningitis vaccine",
+            "international travel",
+            "hajj vaccine",
+            "umrah vaccine",
+            "travel health",
+        ],
+        "official_domains": [
+            "nhsrc.gov.pk",
+            "nih.org.pk",
+            "gov.pk",
+            "who.int",
+        ],
+    },
+
+    "Domicile": {
+        "description": "Domicile certificate application and verification.",
+        "keywords": [
+            "domicile",
+            "domicile certificate",
+            "permanent residence",
+            "district domicile",
+            "residence certificate",
+        ],
+        "official_domains": [
+            "gov.pk",
+            "punjab.gov.pk",
+            "sindh.gov.pk",
+            "kp.gov.pk",
+            "balochistan.gov.pk",
+        ],
+    },
+}
