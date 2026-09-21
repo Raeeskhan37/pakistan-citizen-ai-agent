4d520510651ce08d04507b8777d5216485e6064a
Raeeskhan37 <raees.bscs37@gmail.com>
2026-09-21 19:34:14 +0500

    Create groq_search.py

---
+++ search/groq_search.py
@@ -0,0 +1,10 @@
+def groq_web_search_available():
+
+    """
+    Reserved module for the Groq native web-search /
+    Compound research layer.
+
+    The independent DDGS search layer is used in Version 1.
+    """
+
+    return False
